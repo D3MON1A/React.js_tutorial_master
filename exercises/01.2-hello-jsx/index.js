b9b195cea300a6cacb566358edd112b58c3b3af8
leisyarcia <leisyarcia@gmail.com>
2020-06-15 19:43:52 +0000

    done

--- exercises/01.2-hello-jsx/index.js
+++ exercises/01.2-hello-jsx/index.js
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom";
 
 // if we have a variable
 let age = "12";
-let name = "James";
+let name = "John";
 
 // we can use it in our html like this
 let output = (
-	<span>
-		{name}is{age}years old
-	</span>
+	<span>{ name } is { age } years old</span>
 );
-
 // use react-dom to render it
 ReactDOM.render(output, document.querySelector("#myDiv"));
--- exercises/04.1-conditional-rendering/index.js
+++ exercises/04.1-conditional-rendering/index.js
@@ -4,18 +4,32 @@ import PropTypes from "prop-types";
 
 const Alert = props => {
 	//your component here
-
+	let alertClasses = "alert";
+	if (props.color == "red") {
+		alertClasses += " alert-danger";
+	} else if (props.color == "orange") {
+		alertClasses += " alert-warning";
+	}
+	if (props.proba == "green") {
+		alertClasses += " text-success";
+	}
+	return (
+		<div className={alertClasses} role="alert">
+			{props.text}
+		</div>
+	);
 };
 Alert.propTypes = {
 	color: PropTypes.string,
-	text: PropTypes.string
+	text: PropTypes.string,
+	proba: PropTypes.string
 };
 
 // here is where the alert component is being used, you don't have to edit this part,
 // but it helps you understand what properties are being passed to the component
 ReactDOM.render(
 	<div>
-		<Alert text="OMG! Something really bad has happended!" color="red" />
+		<Alert text="OMG! Something really bad has happended!" color="red" proba="green" />
 		<Alert text="Well, it is not that bad after all!" color="orange" />
 	</div>,
 	document.querySelector("#myDiv")
